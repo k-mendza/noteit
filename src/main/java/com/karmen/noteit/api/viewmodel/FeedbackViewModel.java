ef531923fdb20d835ab3c6f1ddd1a5abb37121a1
Karol Mendza <karolmendza@gmail.com>
2019-08-09 22:21:37 +0200

    Added Feedback

---
+++ src/main/java/com/karmen/noteit/api/viewmodel/FeedbackViewModel.java
@@ -0,0 +1,21 @@
+package com.karmen.noteit.api.viewmodel;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class FeedbackViewModel {
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @NotNull
+    @Min(10)
+    private String feedback;
+}
