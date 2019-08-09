da9895de9923e55ae6d61a41ef0d71798c15a37e
Karol Mendza <karolmendza@gmail.com>
2019-08-09 21:27:21 +0200

    Initial commit

---
+++ src/test/java/com/karmen/noteit/NoteItApplicationTests.java
@@ -0,0 +1,16 @@
+package com.karmen.noteit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoteItApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
