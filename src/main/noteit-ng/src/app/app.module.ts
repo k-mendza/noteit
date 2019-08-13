598ccb1934767ec3ce02561c0046fcb069a34306
Karol Mendza <karolmendza@gmail.com>
2019-08-13 19:06:45 +0200

    Added app routing and other components

--- src/main/noteit-ng/src/app/app.module.ts
+++ src/main/noteit-ng/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {path: 'notes', component: NotesComponent},
+  {path: 'feedback', component: FeedbackComponent},
+  {path: '', component: NotesComponent, pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
